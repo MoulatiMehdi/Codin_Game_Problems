dcf337807f1d9d7662acac82d58176c4bdc8f80c
Mehdi_Moulati <moulati.mehdi@gmail.com>
2023-06-02 20:17:57 +0100

    jest test

--- change-name-files.ps1
+++ change-name-files.ps1
@@ -1,29 +1,3 @@
-$files = Get-ChildItem -Recurse -Path '.\'
-
-Write-Host -ForegroundColor Cyan "Replacing ' ' and '_' Characters with '-'  : "
-
-foreach ($file in $files) {
-
-    if ($file -match "[ _]") {
-
-        $newName = $file -replace "[_ ]+", "-"    
-        $newName = $newName.Trim()
-        $newName = $newName.ToLower()
-
-    
-        <# Action to perform if the condition is true #>
-        try {
-            $null = rename-item -Path $file.FullName -NewName $newName -ErrorAction Stop
-            pass($newName)
-        }
-        catch {
-            <#Do this if a terminating exception happens#>
-            fail -msg "In file '$file' : "
-            Write-Host -ForegroundColor red "`t$err"
-        }
-    }
-    
-}
 
 function msg {
     param(
@@ -32,20 +6,64 @@ function msg {
         [string]$label,
         [string]$text)
     
-    $label = $label.ToUpper()
+    $label = $label.ToUpper().Trim()
     $text = $text.Trim()
+
     Write-Host -ForegroundColor $fg -BackgroundColor $bg " $label " -NoNewline 
-    Write-Host -ForegroundColor White "`t$msg"
+    Write-Host -ForegroundColor $bg "`t$text" -NoNewline
 }
 function pass {
     param ([string]$msg )
-    msg -fg Black -bg Green -label 'Pass' -msg $msg
-}
-function warn {
-    param($msg)
-    msg -fg Black -bg Yellow -label 'WARN' -msg $msg
+    msg -fg Black -bg Green -label 'Pass' -text $msg + "`n"
 }
 function fail {
     param($msg)
-    msg -fg Black -bg Red -label 'fail' -msg $msg
+    msg -fg Black -bg Red -label 'fail' -text $msg
 }
+function warn {
+    param($msg)
+    msg -fg Black -bg Yellow -label 'WARN' -text $msg+"`n"
+}
+
+Write-Host -ForegroundColor White "- Replacing ' ' and '_' Characters with '-'  ...`n"
+
+$local:files = Get-ChildItem -Path ".\" -Recurse  -Exclude 'node_modules' | Sort-Object -Property @{Expression = { $_.PSIsContainer -eq $true } } | Where-Object { 
+    $_.FullName -notlike '*\node_modules\*' -and $_.Name -cmatch "[ _A-Z]+" 
+}
+
+if ($files.Count -eq 0) {
+    Write-Host -ForegroundColor Cyan "All Files are Names Well"
+}
+else {
+
+    foreach ($file in $files) {
+
+        $local:newName = $file.Name -replace "[_ ]+", "-"    
+        $newName = $newName.Trim()
+        $newName = $newName.ToLower()
+
+    
+        <# Action to perform if the condition is true #>
+        try {
+            $null = rename-item -Path $file.FullName -NewName $newName -Force -ErrorAction Stop
+            pass("Target : " + $file.Name)
+            Write-Host -ForegroundColor Gray "`n`tOld Name : "  -NoNewline
+            Write-Host -ForegroundColor White $file.Name 
+            Write-Host -ForegroundColor Cyan "`tNew Name : " -NoNewline
+            Write-Host -ForegroundColor White  $newName
+        }
+        catch {
+            <#Do this if a terminating exception happens#>
+            $local:err = $Error[0]
+            fail( "Failed to Rename the item")
+            Write-Host -ForegroundColor Cyan " '$($file.Name)' :"
+            Write-Host -ForegroundColor red   "`t`t- Error : `t$err"
+            Write-Host -ForegroundColor White "`t`t- Path  : `t$file"
+        }
+    }
+}
+
+
+
+
+#Remove-Variable files ,file  
