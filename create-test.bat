e76762e719e66dcc1d3af6c39715e91ff0f38ad9
Mehdi_Moulati <moulati.mehdi@gmail.com>
2023-06-14 13:58:30 +0100

    add bat files to run commands

---
+++ create-test.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File ".\scripts\create-test-files.ps1" %*
---
+++ rename-files.bat
@@ -0,0 +1,2 @@
+@echo off
+powershell.exe -ExecutionPolicy Bypass -File ".\scripts\rename-files.ps1" %*
