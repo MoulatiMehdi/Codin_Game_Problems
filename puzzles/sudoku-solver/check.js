935374718aa215090aa7fa9db87c29ff4f4e4d50
Mehdi_Moulati <moulati.mehdi@gmail.com>
2023-06-18 12:46:50 +0100

    manually test file

--- puzzles/sudoku-solver/check.js
+++ puzzles/sudoku-solver/check.js
@@ -2,6 +2,7 @@
 
 const fs = require("fs");
 const solve = require("./main");
+const print = require("./print");
 
 function readline(array) {
 	return array.shift();
@@ -17,42 +18,42 @@ fs.readFile(__dirname + "\\test.json", "utf8", (err, data) => {
 		return;
 	}
 	console.info("Json file loaded");
-	// all test cases
+
 	try {
 		const tests = JSON.parse(data)["tests"];
 
 		// the input of the target case
-		if (tests) {
-			const test = tests[3];
+		if (!tests) {
+			throw "Tests json is Empty";
+		}
+		const indexes = [3];
+
+		for (let i of indexes) {
+			const test = tests[i];
 			// for (let test of tests[0]) {
-			console.log(
-				"\t".repeat(5),
-				"\033[1;30;41m",
-				"\t" + test.name + "\t"
-			);
-			console.log("\n");
-			console.log(
-				"\033[1;30;46mInputs         : \033[0m",
-				"\033[1;37m\n",
-				test.input.join('\n')
-			);
+			console.log("\t".repeat(5), "\033[1;30;41m\t" + test.name + "\t\n");
+
+			// get the input as table
+			const origin = test.input.map((c) => c.split(""));
 
 			console.log = mock;
 			solve(() => readline(test.input));
 			console.log = backup;
 
-			console.log(
-				"\033[1;30;47mYour answer    : \033[0m",
-				"\033[1;37m\n" + output
-			);
-			console.log(
-				"\033[1;30;42mCorrect answer : \033[0m",
-				"\033[1;37m\n" + test.output.join("\n")
-			);
-			console.log("\033[1;35m*".repeat(120));
-			// }
-		} else {
-			console.warn("tests variable is empty");
+			const answer = test.output.map((c) => c.split(""));
+			const myAnswer = output.split("\n").map((c) => c.split(""));
+
+			// the input
+			console.log("\n\t\033[1;30;46m  Inputs:  \033[0m\n");
+			print(origin, origin);
+
+			// the correct answer
+			console.log("\n\t\033[1;30;42m  Correct answer:  \033[0m\n");
+			print(answer, origin);
+
+			//my answer
+			console.log("\n\t\033[1;30;46m  My answer:  \033[0m\n");
+			print(myAnswer, origin);
 		}
 	} catch (err) {
 		console.error(err);
