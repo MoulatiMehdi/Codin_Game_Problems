697c79c87118d959c15bcb1f1bad3383d9c1bbc5
Mehdi_Moulati <moulati.mehdi@gmail.com>
2023-06-18 12:49:18 +0100

    test connection before creating folders and add force parameter

--- scripts/create-puzzle.ps1
+++ scripts/create-puzzle.ps1
@@ -1,6 +1,10 @@
 # Parameter help description
 param(
-    [string]$Name
+    [Parameter(Mandatory = $true, Position = 0)]
+    [string]$Name,
+    [Parameter(Mandatory = $false)]
+    [switch]$Force = $false
+
 )
 
 $LastFolder = Get-Location
@@ -26,41 +30,57 @@ $FileJS = (Join-Path $mainPath -ChildPath "js\getAll.js")
 try {
     $isExist = Test-Path $FolderPuzzle
     $isConnected = Test-Connection -ComputerName "www.google.com" -Count 1 -Quiet
-
+   
+    # test if the connection is available
+    if (!$isConnected) {
+        throw "No connextion is available."
+    }
     if ($isExist) {
-        throw "$Name Puzzle already exists in the puzzles Folder."
+        # remove the exist folder if already exist
+        if ($Force) {
+            Remove-Item -Path $FolderPuzzle -Recurse -Force
+        }
+        else {
+            throw "$Name Puzzle already exists in 'puzzles' Folder."
+
+        }
     }
 
-    <# Action when all if and elseif conditions are false #>
+    # create the puzzle folder
     Write-Host "Creating Puzzle Folder ..."
     $null = mkdir -Path $FolderPuzzle
+    # create jest test folder
     Write-Host "`tCreating Tests Folder ..."
     $null = mkdir -Path $FolderTests
 
     Set-Content $FileMain -Value "function solve(readline){`n`n}`n`nmodule.exports = solve"
     Set-Location $FolderPuzzle
 
-    if (!$isConnected) {
-        throw "No connextion is available."
-    }
-        
+    # get the cases and the description form the codinGame website
     node  $FileJS $Name
+    
+    # test json file 
     $isJsonExist = Test-Path $FileJson
     $isJsonEmpty = (get-content $FileJson -raw ) -inotmatch "input"
+   
     if (!$isJsonExist) {
         throw "File Json doesn't exist in the $Name puzzle Folder."
     }
     if ($isJsonEmpty) {
         throw "Json file is Empty"
     }
-    npm run create-test-files --name="$Name" --silent
 
-    Set-Location $LastFolder
+    # create jest test files inside the puzzle folder
+    npm run create-test-files --name="$Name" --silent
 }
 catch {
     <#Do this if a terminating exception happens#>
     fail -Message "Script failed : "
     Write-Host -ForegroundColor Red "`t+ Error : " $Error[0]
+    
+
+}
+finally {
     Set-Location $LastFolder
     exit
 
